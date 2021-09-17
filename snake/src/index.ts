8c75d088a053238ae2839619f477bae911e85e39
Cissy <1395422644@qq.com>
2021-09-17 16:00:17 +0800

    build:snake geme-controller

--- snake/src/index.ts
+++ snake/src/index.ts
@@ -1,17 +1,9 @@
 
 import './index.less'
 
-import Food  from './modules/Food'
-import ScorePanel from './modules/ScorePanel'
+import GameControl from './modules/GameControl'
+
+const gameControl=new GameControl()
 
 
-const food=new Food()
-food.change()
-console.log(food.x,food.y)
 
-//测试代码
-let scorePanel=new ScorePanel()
-scorePanel.addScore()
-scorePanel.addLevel()
-scorePanel.addScore()
-scorePanel.addScore()
--- snake/src/modules/GameControl.ts
+++ snake/src/modules/GameControl.ts
@@ -2,22 +2,68 @@ import Food from "./Food";
 import Snake from "./Snake";
 import ScorePanel from "./ScorePanel";
 
-class GameControl{
-    food:Food;
-    snake:Snake;
-    scorePanel:ScorePanel;
+class GameControl {
+    food: Food;
+    snake: Snake;
+    scorePanel: ScorePanel;
+    direction:string='';
+    isLive=true
 
-    constructor(){
-        this.food=new Food()
-        this.snake=new Snake()
-        this.scorePanel=new ScorePanel()
+    constructor() {
+        this.food = new Food()
+        this.snake = new Snake()
+        this.scorePanel = new ScorePanel()
         this.init()
     }
 
     //初始化游戏，表示游戏开始
-    init(){}
-    //
-    
+    init() {
+        document.addEventListener('keydown', this.keydownHandler.bind(this))
+        this.run()
+    }
+    //按键方法
+    // ArrowDown
+    // ArrowRight
+    // ArrowUp
+    // ArrowLeft
+    keydownHandler(event: KeyboardEvent){
+        console.log(event.key)
+        this.direction=event.key
+        
+    }
+
+    run(){
+
+
+        let X=this.snake.X
+        let Y=this.snake.Y
+        switch(this.direction){
+            case"ArrowDown":
+            case"Down":
+            Y+=10
+            break
+            case"ArrowRight":
+            case"Right":
+            X+=10
+            break
+            case"ArrowUp":
+            case"Up":
+            Y-=10
+            break
+            case"ArrowLeft":
+            case"Left":
+            X-=10 
+            break
+        }
+        this.snake.X=X
+        this.snake.Y=Y
+
+        // setTimeout(this.run.bind(this),300)
+        if(this.isLive){
+            setTimeout(this.run.bind(this),300-(this.scorePanel.level-1)*30)
+        }
+    }
+
 }
 
 export default GameControl
